fe55f6975cb09e56fdaa24bde17d5eee58e1e2a3
Kyle Uy <kyleeeee09@gmail.com>
2025-07-09 17:52:40 +0800

    feat: added filters

--- src/components/Registration/Individual.jsx
+++ src/components/Registration/Individual.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 import EditForm from "../EditForm";
+import Swal from "sweetalert2";
+import supabase from "../../utils/supabase";
 
 const Individual = ({ filteredUsers = [] }) => {
   const [expandedRows, setExpandedRows] = useState(new Set());
   const [editRegistration, setEditRegistration] = useState(null);
+  const [activePaymentStatus, setActivePaymentStatus] = useState(null);
+  const [activeTraining, setActiveTraining] = useState(null);
 
   function formatCurrency(amount) {
     const num = parseFloat(amount);
@@ -14,14 +18,49 @@ const Individual = ({ filteredUsers = [] }) => {
       currency: "USD",
     });
   }
-  function toggleRow(idx) {
-    setExpandedRows((prev) => {
-      const next = new Set(prev);
-      if (next.has(idx)) next.delete(idx);
-      else next.add(idx);
-      return next;
+
+  const addedFilteredUsers = filteredUsers.filter((user) => {
+    const paymentStatusMatches =
+      !activePaymentStatus || user.payment_status === activePaymentStatus;
+    const trainingMatches =
+      !activeTraining || user.trainings.includes(activeTraining);
+    return paymentStatusMatches && trainingMatches;
+  });
+
+  async function handleDelete(id) {
+    const result = await Swal.fire({
+      title: "Are you sure?",
+      text: "Do you really want to delete this registration?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
     });
+
+    if (result.isConfirmed) {
+      const { error: trainRefError } = await supabase
+        .from("training_references")
+        .delete()
+        .eq("registration_id", id);
+
+      const { error: regError } = await supabase
+        .from("registrations")
+        .delete()
+        .eq("id", id);
+
+      if (regError || trainRefError || attendeeError) {
+        Swal.fire(
+          "Error!",
+          "There was a problem deleting the registration.",
+          "error"
+        );
+      } else {
+        Swal.fire("Deleted!", "The guest has been deleted.", "success");
+      }
+    }
   }
+
   return (
     <>
       <div className="table-responsive">
@@ -37,75 +76,163 @@ const Individual = ({ filteredUsers = [] }) => {
                 <th>Position</th>
                 <th>Designation</th>
                 <th>Country</th>
-                <th>Trainings</th>
+                <div className="dropdown w-100 text-center">
+                  <button
+                    className="dropdown-toggle"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                    style={{
+                      backgroundColor: "transparent",
+                      height: "55px",
+                      border: "none",
+                    }}
+                  >
+                    <th> Trainings</th>
+                  </button>
+                  <ul className="dropdown-menu dropdown-menu-dark ">
+                    {[
+                      "Annual Pacific Region Investment Conference",
+                      "Applied Responsible Investment for Fiduciaries",
+                      "Accredited Investment Fiduciary Training",
+                      "Responsible Investment Essentials",
+                      "Investment Governance Essentials",
+                    ].map((training, index) => (
+                      <li key={index}>
+                        <a
+                          className="dropdown-item"
+                          onClick={() => setActiveTraining(training)}
+                        >
+                          {training}
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
                 <th>Total Cost</th>
-                <th>Payment Status</th>
+                <div className="dropdown">
+                  <button
+                    class="dropdown-toggle"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                    style={{
+                      backgroundColor: "transparent",
+                      height: "55px",
+                      border: "none",
+                    }}
+                  >
+                    <th>Payment Status</th>
+                  </button>
+                  <ul className="dropdown-menu dropdown-menu-dark">
+                    <li>
+                      <a
+                        className="dropdown-item"
+                        onClick={() => setActivePaymentStatus("Unpaid")}
+                      >
+                        Unpaid
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="dropdown-item"
+                        onClick={() => setActivePaymentStatus("Paid")}
+                      >
+                        Paid
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="dropdown-item"
+                        onClick={() => setActivePaymentStatus("")}
+                      >
+                        No Filter
+                      </a>
+                    </li>
+                  </ul>
+                </div>
                 <th className="text-center " colSpan={2}>
                   Actions
                 </th>
               </tr>
             </thead>
             <tbody>
-              {filteredUsers.map((reg, i) => {
-                const dateObj = new Date(reg.submission_date);
+              {addedFilteredUsers.length === 0 ? (
+                <tr>
+                  <td colSpan={12} className="text-center">
+                    <h1 className="m-5"> No records satisfy the filter</h1>
+                  </td>
+                </tr>
+              ) : (
+                addedFilteredUsers.map((reg, i) => {
+                  const dateObj = new Date(reg.submission_date);
 
-                const options = {
-                  year: "numeric",
-                  month: "long",
-                  day: "numeric",
-                  hour: "numeric",
-                  minute: "2-digit",
-                  hour12: true,
-                };
+                  const options = {
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                    hour: "numeric",
+                    minute: "2-digit",
+                    hour12: true,
+                  };
 
-                const formattedDate = dateObj.toLocaleString("en-US", options);
-                return (
-                  <tr key={i}>
-                    <td className="small">{reg.company}</td>
-                    <td className="small">{formattedDate}</td>
-                    <td className="small">{reg.first_name}</td>
-                    <td className="small">{reg.last_name}</td>
-                    <td className="small">{reg.email}</td>
-                    <td className="small">{reg.position}</td>
-                    <td className="small">{reg.designation}</td>
-                    <td className="small">{reg.country}</td>
-                    <td className="small">
-                      {(reg.training_references || [])
-                        .map((tr) =>
-                          tr.trainings
-                            ? `${tr.trainings.name} (${tr.trainings.date})`
-                            : null
-                        )
-                        .filter(Boolean)
-                        .join(", ")}
-                    </td>
-                    <td className="small">{formatCurrency(reg.total_cost)}</td>
-                    <td className="small">{reg.payment_status}</td>
-                    <td colSpan={2} className="sticky-col">
-                      <div className="btn-group">
-                        <button
-                          className="btn"
-                          data-bs-toggle="modal"
-                          data-bs-target="#editModal"
-                          onClick={() => setEditRegistration(reg)}
-                        >
-                          <i class="bi bi-pencil-square" />
-                        </button>
-                        <button
-                          className="btn"
-                          // onClick={() => deleteGuest(guest.id)}
-                        >
-                          <i class="bi bi-trash-fill" />
-                        </button>
-                      </div>
-                    </td>
-                  </tr>
-                );
-              })}
+                  const formattedDate = dateObj.toLocaleString(
+                    "en-US",
+                    options
+                  );
+                  return (
+                    <tr key={i}>
+                      <td className="small">{reg.company}</td>
+                      <td className="small">{formattedDate}</td>
+                      <td className="small">{reg.first_name}</td>
+                      <td className="small">{reg.last_name}</td>
+                      <td className="small">{reg.email}</td>
+                      <td className="small">{reg.position}</td>
+                      <td className="small">{reg.designation}</td>
+                      <td className="small">{reg.country}</td>
+                      <td className="small">
+                        <ul className=" mb-0">
+                          {(reg.training_references || [])
+                            .map((tr) =>
+                              tr.trainings ? (
+                                <li key={tr.trainings.id}>
+                                  {tr.trainings.name}
+                                </li>
+                              ) : null
+                            )
+                            .filter(Boolean)}
+                        </ul>
+                      </td>
+                      <td className="small">
+                        {formatCurrency(reg.total_cost)}
+                      </td>
+                      <td className="small">{reg.payment_status}</td>
+                      <td colSpan={2} className="sticky-col">
+                        <div className="btn-group">
+                          <button
+                            className="btn"
+                            data-bs-toggle="modal"
+                            data-bs-target="#editModal"
+                            onClick={() => setEditRegistration(reg)}
+                          >
+                            <i class="bi bi-pencil-square" />
+                          </button>
+                          <button
+                            className="btn"
+                            onClick={() => handleDelete(reg.id)}
+                          >
+                            <i class="bi bi-trash-fill" />
+                          </button>
+                        </div>
+                      </td>
+                    </tr>
+                  );
+                })
+              )}
             </tbody>
           </table>
         </div>
       </div>
+
+      {/* Edit Modal */}
       <div>
         <div
           className="modal fade"
