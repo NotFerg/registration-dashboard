c2c2fbe275cf93a5184e92c26dbbd36736f0a952
Kyle Uy <kyleeeee09@gmail.com>
2025-07-15 10:07:16 +0800

    feat: Added swals for each actions

--- src/components/AddMultiPageModal.jsx
+++ src/components/AddMultiPageModal.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Modal, Button } from "react-bootstrap";
 import AddAttendees from "./addAttendees";
 import supabase from "../utils/supabase";
+import Swal from "sweetalert2";
 const AddMultiPageModal = ({ show, onHide }) => {
   const attendeeTemplate = {
     first_name: "",
@@ -74,15 +75,6 @@ const AddMultiPageModal = ({ show, onHide }) => {
     setStep(Math.max(step - 1, 0));
   }
 
-  // function handleSaveGroup(e) {
-  //   // e.preventDefault();
-  //   // console.log("Registration Info:", reg);
-  //   // console.log("Attendees:", attendees);
-  //   // console.log("Full Group Registration:", { ...reg, attendees });
-  //   // onHide();
-
-  // }
-
   async function handleSaveGroup(e) {
     e.preventDefault();
     console.log("Full Group Registration:", { ...reg, attendees });
@@ -141,71 +133,82 @@ const AddMultiPageModal = ({ show, onHide }) => {
 
       if (attendeeError) throw attendeeError;
 
-      console.log("✅ Group registration successfully saved.");
-      onHide(); // close modal
+      Swal.fire({
+        title: "Group registration successfully saved!",
+        icon: "success",
+        confirmButtonText: "OK",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          window.location.reload();
+        }
+      });
     } catch (error) {
-      console.error("❌ Error saving registration:", error);
-      alert(
-        "There was an error saving the registration. Please try again.",
-        error
-      );
+      Swal.fire({
+        title: "Error!",
+        text: "There was an error saving the registration. Please try again.",
+        icon: "error",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          window.location.reload();
+        }
+      });
     }
   }
 
   return (
-    <Modal show={show} onHide={onHide} size='lg'>
+    <Modal show={show} onHide={onHide} size="lg">
       <Modal.Header closeButton>
         <Modal.Title>
           <h3>Add Group Registration</h3>
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <div className='mb-3'>
-          <label htmlFor='company' className='form-label'>
+        <div className="mb-3">
+          <label htmlFor="company" className="form-label">
             Company <span style={{ color: "red" }}> * </span>
           </label>
           <input
-            type='text'
-            className='form-control'
-            id='company'
-            name='company'
-            placeholder='Enter Company Name'
-            aria-describedby='company'
+            type="text"
+            className="form-control"
+            id="company"
+            name="company"
+            placeholder="Enter Company Name"
+            aria-describedby="company"
             onChange={handleChange}
             value={reg.company}
             required
           />
         </div>
 
-        <div className='d-flex flex-row justify-content-between'>
-          <div className='mb-3'>
-            <label htmlFor='first_name' className='form-label'>
+        <div className="d-flex flex-row justify-content-between">
+          <div className="mb-3">
+            <label htmlFor="first_name" className="form-label">
               First Name <span style={{ color: "red" }}> * </span>
             </label>
             <input
-              type='text'
-              className='form-control'
-              id='first_name'
-              name='first_name'
-              placeholder='Enter First Name'
-              aria-describedby='first_name'
+              type="text"
+              className="form-control"
+              id="first_name"
+              name="first_name"
+              placeholder="Enter First Name"
+              aria-describedby="first_name"
               onChange={handleChange}
               value={reg.first_name}
               required
             />
           </div>
 
-          <div className='mb-3'>
-            <label htmlFor='last_name' className='form-label'>
+          <div className="mb-3">
+            <label htmlFor="last_name" className="form-label">
               Last Name <span style={{ color: "red" }}> * </span>
             </label>
             <input
-              type='text'
-              className='form-control'
-              id='last_name'
-              name='last_name'
-              placeholder='Enter Last Name'
-              aria-describedby='last_name'
+              type="text"
+              className="form-control"
+              id="last_name"
+              name="last_name"
+              placeholder="Enter Last Name"
+              aria-describedby="last_name"
               onChange={handleChange}
               value={reg.last_name}
               required
@@ -213,34 +216,34 @@ const AddMultiPageModal = ({ show, onHide }) => {
           </div>
         </div>
 
-        <div className='mb-3'>
-          <label htmlFor='email' className='form-label'>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
             Email <span style={{ color: "red" }}> * </span>
           </label>
           <input
-            type='email'
-            className='form-control'
-            id='email'
-            name='email'
-            placeholder='Enter Email'
-            aria-describedby='email'
+            type="email"
+            className="form-control"
+            id="email"
+            name="email"
+            placeholder="Enter Email"
+            aria-describedby="email"
             onChange={handleChange}
             value={reg.email}
             required
           />
         </div>
 
-        <div className='mb-3'>
-          <label htmlFor='total_cost' className='form-label'>
+        <div className="mb-3">
+          <label htmlFor="total_cost" className="form-label">
             Total Cost <span style={{ color: "red" }}> * </span>
           </label>
           <input
-            type='number'
-            className='form-control'
-            id='total_cost'
-            name='total_cost'
-            placeholder='Enter Total Cost'
-            aria-describedby='total_cost'
+            type="number"
+            className="form-control"
+            id="total_cost"
+            name="total_cost"
+            placeholder="Enter Total Cost"
+            aria-describedby="total_cost"
             onChange={handleChange}
             value={reg.total_cost}
             readOnly
@@ -248,39 +251,39 @@ const AddMultiPageModal = ({ show, onHide }) => {
           />
         </div>
 
-        <div className='mb-3'>
-          <label htmlFor='payment_options' className='form-label'>
+        <div className="mb-3">
+          <label htmlFor="payment_options" className="form-label">
             Payment Options <span style={{ color: "red" }}> * </span>
           </label>
           <input
-            type='text'
-            className='form-control'
-            id='payment_options'
-            name='payment_options'
-            placeholder='Enter Full Payment Options'
-            aria-describedby='payment_options'
+            type="text"
+            className="form-control"
+            id="payment_options"
+            name="payment_options"
+            placeholder="Enter Full Payment Options"
+            aria-describedby="payment_options"
             onChange={handleChange}
             value={reg.payment_options}
             required
           />
         </div>
 
-        <div className='mb-3'>
-          <label htmlFor='payment_status' className='form-label'>
+        <div className="mb-3">
+          <label htmlFor="payment_status" className="form-label">
             Payment Status <span style={{ color: "red" }}> * </span>
           </label>
           <select
-            className='form-select'
-            id='payment_status'
-            name='payment_status'
-            aria-describedby='payment_status'
+            className="form-select"
+            id="payment_status"
+            name="payment_status"
+            aria-describedby="payment_status"
             onChange={handleChange}
             value={reg.payment_status}
             required
           >
-            <option value=''>Select Payment Status</option>
-            <option value='Paid'>Paid</option>
-            <option value='Unpaid'>Unpaid</option>
+            <option value="">Select Payment Status</option>
+            <option value="Paid">Paid</option>
+            <option value="Unpaid">Unpaid</option>
           </select>
         </div>
         <hr />
@@ -291,9 +294,9 @@ const AddMultiPageModal = ({ show, onHide }) => {
         />
       </Modal.Body>
       <Modal.Footer>
-        <div className='w-100 d-flex justify-content-between'>
+        <div className="w-100 d-flex justify-content-between">
           <Button
-            variant='secondary'
+            variant="secondary"
             onClick={handlePrev}
             disabled={step === 0}
           >
@@ -303,10 +306,10 @@ const AddMultiPageModal = ({ show, onHide }) => {
             {/* Attendee {attendees.length || 1} of {step + 1} */}
             Attendee {step + 1} of {attendees.length || 1}
           </small>
-          <Button variant='primary' onClick={handleNext}>
+          <Button variant="primary" onClick={handleNext}>
             Next
           </Button>
-          <Button variant='success' onClick={handleSaveGroup}>
+          <Button variant="success" onClick={handleSaveGroup}>
             Save Group Registration
           </Button>
         </div>
